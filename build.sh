aa631d7784ed8f05c4a60a23a49ffe1aa859f5a4
Jane Mercer <radix42@gmail.com>
2019-06-11 17:58:26 -0700

    update build.sh to use depends and add libsodium

--- build.sh
+++ build.sh
@@ -2,29 +2,13 @@
 # EMC2 build script for Ubuntu & Debian 9 v.3 (c) Decker (and webworker)
 # modified for CHIPS by Duke Leto
 
-berkeleydb () {
     CHIPS_ROOT=$(pwd)
-    CHIPS_PREFIX="${CHIPS_ROOT}/db4"
-    mkdir -p $CHIPS_PREFIX
-    wget -N 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-    echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef db-4.8.30.NC.tar.gz' | sha256sum -c
-    tar -xzvf db-4.8.30.NC.tar.gz
-    cd db-4.8.30.NC/build_unix/
-
-    ../dist/configure -enable-cxx -disable-shared -with-pic -prefix=$CHIPS_PREFIX
-
-    make install
-    cd $CHIPS_ROOT
-}
-
-buildchips () {
     git pull
     make clean
+    cd depends
+    make NO_QT=1
+    cd ..
     ./autogen.sh
-    ./configure LDFLAGS="-L${CHIPS_PREFIX}/lib/" CPPFLAGS="-I${CHIPS_PREFIX}/include/" --with-gui=no --disable-tests --disable-bench --without-miniupnpc --enable-experimental-asm --enable-static --disable-shared
-    make -j$(nproc)
-}
+CONFIG_SITE="$PWD/depends/x86_64-pc-linux-gnu/share/config.site"    ./configure LDFLAGS="-L${CHIPS_ROOT}/depends/x86_64-pc-linux-gnu/lib" CPPFLAGS="-I${CHIPS_ROOT}/depends/x86_64-pc-linux-gnu//include" --with-gui=no --disable-tests --disable-bench --without-miniupnpc --enable-experimental-asm --enable-static --disable-shared --with-boost-libdir=$(pwd)/depends/x86_64-pc-linux-gnu/lib 
+    make #  -j$(nproc)
 
-berkeleydb
-buildchips
-echo "Done building CHIPS!"
