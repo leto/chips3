3f58101a570d37a72f8f1489c9efc0f57c74cb3a
Jane Mercer <radix42@gmail.com>
2019-06-12 19:22:00 -0700

    initial version with verushash

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# EMC2 build script for Ubuntu & Debian 9 v.3 (c) Decker (and webworker)
+# modified for CHIPS by Duke Leto
+
+    CHIPS_ROOT=$(pwd)
+#    git pull
+    make clean
+    cd depends
+    make NO_QT=1
+    cd ..
+    ./autogen.sh
+CONFIG_SITE="$PWD/depends/x86_64-pc-linux-gnu/share/config.site"    ./configure LDFLAGS="-L${CHIPS_ROOT}/depends/x86_64-pc-linux-gnu/lib" CPPFLAGS="-I${CHIPS_ROOT}/depends/x86_64-pc-linux-gnu//include" --with-gui=no --disable-tests --disable-bench --without-miniupnpc --enable-experimental-asm --enable-static --disable-shared --with-boost-libdir=$(pwd)/depends/x86_64-pc-linux-gnu/lib 
+    make V=1 #  -j$(nproc)
+
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,4 @@
-packages:=boost openssl libevent zeromq
-native_packages := native_ccache
+packages:=boost openssl libevent zeromq libsodium
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
